afad5b78df8d8ecca6c5d16e1114e1153bb4d270
StellarSoup <kolpiou@live.com>
2016-08-14 10:37:12 +1000

    Added Shader Updated Project Version and added More components

---
+++ MUS302-MaxTest Project/Assets/ProxPlayerPosUpdate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ProxPlayerPosUpdate : MonoBehaviour {
+
+    public Transform player;
+
+    public bool glow;
+	
+	// Update is called once per frame
+	void Update () {
+        if (player != null)
+        {
+            // Pass the player location to the shader
+            GetComponent<Renderer>().sharedMaterial.SetVector("_PlayerPosition", player.position);
+            if (glow)
+            {
+                GetComponent<Renderer>().sharedMaterial.SetColor("_OutlineColour", new Vector4(1, 1, 1, 1));
+            }
+        }
+    }
+}
